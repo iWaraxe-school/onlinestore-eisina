4c132ee6926a0e282d26b5270164be76b8bd9ad7
cath isina <isinacath378@gmail.com>
2022-01-10 10:42:22 +0300

    Udating main method

--- parent/consoleApp/src/main/java/StoreApp.java
+++ parent/consoleApp/src/main/java/StoreApp.java
@@ -1,9 +1,6 @@
 import org.reflections.Reflections;
 
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.lang.reflect.*;
 
 import static org.reflections.scanners.Scanners.SubTypes;
@@ -14,7 +11,25 @@ public class StoreApp {
         StoreHelper helper = new StoreHelper();
         Store onlineStore = new Store();
         helper.populateStore(onlineStore);
-        onlineStore.printAllCategoriesAndProduct();
+    //    onlineStore.printAllCategoriesAndProduct();
+
+        while (true)
+        {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter text: ");
+            String option = input.next();
+            switch (option){
+                case "sort":
+                    System.out.println("Soring the catalog");
+                    break;
+                case "top":
+                    System.out.println("The top 5 products are:");
+                    break;
+                case "quit":
+                    return;
+            }
+            System.out.println();
+        }
     }
 }
 
